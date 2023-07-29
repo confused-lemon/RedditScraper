9111edd62a24f8c5ac653977a49eaffd80af1273
Joe <joe@debian-BULLSEYE-live-builder-AMD64>
2023-07-29 15:33:53 -0400

    Script ready for deployment

--- bin/snapshot_col_gen.py
+++ bin/snapshot_col_gen.py
@@ -5,19 +5,23 @@ from datetime import datetime
 path_to_file = os.path.dirname(os.path.dirname(__file__))
 
 file_list = [file for file in os.listdir(f'{path_to_file}/CSV_datasets')]
-path = '/home/lighthouse/Desktop/Code/Projects/RedditScraper/CSV_datasets/20230623_211502.csv'
+print(f'Found {len(file_list)} files for review')
+changed = 0
+skipped = 0
+for file in file_list:
+    fileloc = f'{path_to_file}/CSV_datasets/{file}'
+    df = pd.read_csv(fileloc)
+    headers = df.columns.tolist()
+    if 'snapshot_time(UTC)' in headers:
+        skipped += 1
+        continue
+    else:
+        changed += 1
+        stamp = file.replace('.csv', '')
+        datetime_obj = datetime.strptime(stamp, "%Y%m%d_%H%M%S")
+        new_stamp = datetime_obj.strftime("%Y-%m-%d %H:%M:%S")
+        df["snapshot_time(UTC)"] = new_stamp
+        df.to_csv(f'{path_to_file}/CSV_datasets/{stamp}.csv',index=False)
 
-# for file in file_list:
-#     stamp = file.replace('.csv', '')
-df = pd.read_csv(path)
-headers = df.columns.tolist()
-file = '20230623_211502.csv'
-
-if 'snapshot_time(UTC)' in headers:
-    pass
-else:
-    stamp = file.replace('.csv', '')
-    datetime_obj = datetime.strptime(stamp, "%Y%m%d_%H%M%S")
-    new_stamp = datetime_obj.strftime("%Y-%m-%d %H:%M:%S")
-    df["snapshot_time(UTC)"] = new_stamp
-    # df.to_csv(,index=False)
+print(f'Changed {changed} files')
+print(f'Skipped {skipped} files')
