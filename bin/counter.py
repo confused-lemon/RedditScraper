78b8dd03f2927c1af92d16f9b4ddaae54818cb47
Joe <jvwalsh@protonmail.com>
2024-08-12 15:47:29 -0400

    Removing deleted data files

--- bin/counter.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-def count_csv_rows(file_path) -> bool:
-    result = subprocess.run(f'wc -l {file_path}'.split(' '), capture_output=True, text=True)
-    rec_count = int(result.stdout.split()[0]) - 1
-    return (rec_count == 100, rec_count)
-
