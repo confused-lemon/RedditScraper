7d9705465e276955a8ac3305823ae73bd70aa650
Joe <jvwalsh@protonmail.com>
2024-08-26 22:27:20 -0400

    Returns count rather than raising a ValueError

--- bin/utils/counter.py
+++ bin/utils/counter.py
@@ -2,8 +2,6 @@ import subprocess
 
 def count_csv_rows(file_path) -> int:
     """Returns the number of records in a file, ignoring header row\n
-    Expected return is 100, otherwise will raise value error"""
+    Expected return is 100, otherwise will raise assert error"""
     result = subprocess.run(f'wc -l {file_path}'.split(' '), capture_output=True, text=True)
-    row_count = int(result.stdout.split()[0]) - 1
-    if row_count != 100:
-        raise ValueError(f'Row count check failed. Expected 100, got {row_count}') 
+    return int(result.stdout.split()[0]) - 1
