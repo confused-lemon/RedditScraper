740c26969a4d1ac60ac2f679c40237de980a2c59
Joe <jvwalsh@protonmail.com>
2024-09-05 22:30:34 -0400

    Refactored to be called from upload method if needed

--- bin/utils/count_fixer.py
+++ bin/utils/count_fixer.py
@@ -1,9 +1,13 @@
 import re, io
-from glob import glob
 import pandas as pd
 from counter import count_csv_rows
 
-def process_csv(input_file, output_file, match_value=None, regex_pattern=None):
+def process_csv(input_file) -> None:
+    """Takes a file with a bad record count, and fixes it to be 100 records"""
+    
+    match_value = 'snapshot_time(UTC)'
+    regex_pattern = r'(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})\s'
+
     with open(input_file, 'r') as f:
         content = f.read().replace('\n', ' ')
 
@@ -11,22 +15,8 @@ def process_csv(input_file, output_file, match_value=None, regex_pattern=None):
         content = content.replace(match_value, f'{match_value}\n')
     
     if regex_pattern:
-        content = re.sub(f'({regex_pattern})', r'\1\n', content)
+        content = re.sub(regex_pattern, r'\1\n', content)
     
     mock_csv = io.StringIO(content)
     df = pd.read_csv(mock_csv)
-    df.to_csv(output_file, index=False)
-
-files = glob('Error_Files/counts/*.csv')
-for file in files:
-    input_file = file
-    output_file = file
-    match_value = 'snapshot_time(UTC)'
-    regex_pattern = r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}'
-
-    process_csv(input_file, output_file, match_value, regex_pattern)
-
-    try:
-        assert count_csv_rows(output_file) == 100
-    except AssertionError:
-        print(f'{output_file} needs attention')
+    df.to_csv(input_file, index=False)
