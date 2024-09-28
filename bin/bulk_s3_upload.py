1baa4d19c6cbb9e41dd34ae545e083cd06258709
Joe <jvwalsh@protonmail.com>
2024-09-28 12:42:57 -0400

    Added logging and better error handling

--- bin/bulk_s3_upload.py
+++ bin/bulk_s3_upload.py
@@ -1,9 +1,13 @@
-import boto3, os, sys, yaml, glob
+import boto3, os, sys, yaml, glob, logging, logging.config
+import subprocess as sp
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 project_root = os.path.abspath(os.path.join(current_dir, '..'))
 sys.path.append(project_root)
 path = os.path.dirname(os.path.dirname(__file__))
+from cfg.logging_config import LOGGING_CONFIG
+logging.config.dictConfig(LOGGING_CONFIG)
+logging.getLogger('history_logger')
 
 with open(f'{path}/cfg/credentials.yaml','r') as cred_file:
     credentials = yaml.safe_load(cred_file)
@@ -19,8 +23,18 @@ s3_session = boto3.client(
     )
 
 history_files = glob.glob('Processed_files/*.csv')
-
+print(f'Files to upload: {len(history_files)}')
+errs =[]
+err_count=0
 for file in history_files:
-    file_name = file.split('/')[-1]
-    year, month,day = file_name[0:4], file_name[4:6], file_name[6:8]
-    s3_session.upload_file(file, s3_bucket, f'{year}/{month}/{day}/{file_name}')
+    try:
+        file_name = file.split('/')[-1]
+        year, month,day = file_name[0:4], file_name[4:6], file_name[6:8]
+        s3_session.upload_file(file, s3_bucket, f'{year}/{month}/{day}/{file_name}')
+    except Exception as e: 
+        err_count += 1
+        logging.error(f'Failed to upload {file_name}. Reason: {e}')
+        sp.run(['mv', f'{file}', f'{path}/Error_Files/History_upload'], check=True)
+
+
+print(f'Errors: {err_count}')
